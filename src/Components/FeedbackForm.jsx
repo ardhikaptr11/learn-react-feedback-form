3ad9715ff1cfac8daec8568c7b56c8047dddd8b8
Ardhika Putra <ardhikaptr11@gmail.com>
2024-08-10 11:35:41 +0700

    Removing 'nav' tag

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -40,7 +40,6 @@ const FeedbackForm = () => {
 
 	return (
 		<>
-			<nav>Tell Us What You Think</nav>
 			<form className="feedback-form" onSubmit={handleSubmit}>
 				<h2>We'd Love to Hear From You!</h2>
 				<p>Please share your feedback with us.</p>
